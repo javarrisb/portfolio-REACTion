cd51fdc47280ccde9fa967d0befd27d32b7c61c9
Javarris Barnett <javarrisb@gmail.con>
2022-03-05 23:28:58 +0100

    added the about me page

---
+++ src/components/About.js/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import aboutpic from "../../assets/images/aboutpic.jpg";
+
+
+function About() {
+    return(
+        <section className="container-aboutme">
+            <h1 className="header-ab">About Me</h1>
+            <img src={aboutpic} className="mypic" style={{ width: "50%" }}
+            alt="mypicture" />
+            <div className="aboutme-text">
+                <p>
+                    My name is Javarris Barnett and I am embarking on the journey of becoming a full stack web developer. I am attending UNC Chapel Hill Coding bootcamp and love developing innovative web applications with new, cutting edge technology as the ones in this portfolio. When I am not coding and developing, I really adore traveling, reading and outdoor activities with the family and pet dog. 
+                    </p>
+            </div>
+        
+        </section>
+    );
+}
+
+export default About;
