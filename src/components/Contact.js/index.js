f872196026eac9aea099f04a45f2f675ffc16cc6
Javarris Barnett <javarrisb@gmail.con>
2022-03-10 07:43:08 -0500

    updated resume

--- src/components/Contact.js/index.js
+++ src/components/Contact.js/index.js
@@ -2,30 +2,30 @@ import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
-    const [ errorMessage, setErrorMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     function handleChange(e) {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
             console.log(isValid);
             if (!isValid) {
-                setErrorMessage('Your email is invalid');
+                setErrorMessage("Email address is needed!");
             } else {
                 setErrorMessage('');
             }
         }
         else {
             if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
+                setErrorMessage(`${e.target.name} is needed!`);
             } else {
                 setErrorMessage('');
             }
         }
-        setFormState({...formState, [e.target.name]: e.target.value })
+        setFormState({ ...formState, [e.target.name]: e.target.value })
         if (!errorMessage) {
-            setFormState({...formState, [e.target.name]: e.target.value });
+            setFormState({ ...formState, [e.target.name]: e.target.value });
         }
     }
 
@@ -34,27 +34,31 @@ function Contact() {
         console.log(formState);
     }
 
-    return(
-        <section>
-            <h1>Contact me</h1>
-            <form id='contact-form' onSubmit={handleSubmit}>
+    return (
+        <div className='contact-container'>
+            <form className='contact-form' onSubmit={handleSubmit}>
+                <h1 className='header-contact'>Contact me</h1>
                 <div>
-                    <label htmlFor='="name'>Name:</label>
-                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
+                    <label htmlFor='="name' className='label-input'>Name:</label>
+                    <input type="text" className='input-form' defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+                    <label htmlFor='email' className='label-input'>Email:</label>
+                    <input type="text" className='input-form' defaultValue={email} onBlur={handleChange} email="email" />
+                </div>
+                <div>
+                    <label htmlFor="message" className='label-input'>Message:</label>
+                    <textarea name="message" className='input-form' defaultValue={message} onBlur={handleChange} rows="5" />
                 </div>
                 {errorMessage && (
                     <div>
-                        <p className='error-text'>{errorMessage}</p>
+                        <p className='text-error'>{errorMessage}</p>
                     </div>
                 )}
-                <button type="submit">Submit</button>
+                <button className='btn' type="submit" onCLick={handleSubmit}>Submit</button>
             </form>
-        </section>
-    )
-}
+        </div>
+    );
+};
 
 export default Contact;
--- src/components/Resume.js/index.js
+++ src/components/Resume.js/index.js
@@ -1,36 +1,16 @@
 import React from 'react';
+import resume from '../../assets/images/Resume.png';
 
 function Resume() {
     return (
         <div className='resume-container'>
-            <h1 className='resume-heading'>
+            <h1 className='header-resume'>
                 Resume
             </h1>
-            <div className='resume-menu'>
-                <div className='skills'>
-                    <h3 className='develop'>
-                        Front-End Skills
-                    </h3>
-                    <ul className='skills-list'>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                    </ul>
-                </div>
-                <div className='skills'>
-                    <h3 className='develop'>
-                        Back-End Skills
-                    </h3>
-                    <ul className='skills-list'>
-                        <li>Node.js</li>
-                        <li>Express.js</li>
-                        <li>MySQL</li>
-                        <li>MongoDB</li>
-                    </ul>
-                </div>
-                <a href="resume/Resume.pdf" download="Javarris Barnett Resume">Click here to download my resume</a>
+            <div>
+                <h2>Front End Skills</h2>
             </div>
+            <a href="resume/Resume.pdf" className='link-resume' download="Javarris Barnett Resume">Click here to download my resume</a>
         </div>
     )
 }
