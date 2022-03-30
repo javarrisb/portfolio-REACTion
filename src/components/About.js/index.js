5d55a51891f150791dd9b72787cbae4e20fad75c
Javarris Barnett <javarrisb@gmail.con>
2022-03-30 10:43:32 -0400

    updated portfolio

--- src/components/About.js/index.js
+++ src/components/About.js/index.js
@@ -5,14 +5,14 @@ import aboutpic from "../../assets/images/aboutpic.jpg";
 function About() {
     return (
         <section className="container-aboutme">
-            <h1 className="header-ab">About Me</h1>
+            <h1>About Me</h1>
             <div className='picture'>
                 <img src={aboutpic} className="mypic" style={{ width: "50%" }}
                     alt="mypicture" />
             </div>
             <div className="aboutme-text">
                 <p>
-                    My name is Javarris Barnett, born and raised in Charlotte, NC. I am a people person and team oriented person with my background being professional basketball and real estate agent. Switching careers and going through UNC Coding Bootcamp was an experience that taught me not only technolgy skills and languages but also life skills that I will implement daily in my career.Becoming a full stack developer has been a very satisfying journey in my life and I hope to continue learning and developing applications that will help make users lives better. When I am not coding and developing, you can find me traveling, reading and outdoor activities with the family and pet dog.
+                    My name is Javarris Barnett, born and raised in Charlotte, NC. I am a team oriented person with my background being professional basketball player and real estate agent. Switching careers and going through UNC Coding Bootcamp was an experience that taught me not only technolgy skills but also life skills that I will implement daily in my career. Becoming a full stack developer has been a very satisfying journey in my life and will continue learning and developing applications that will help make users lives better. When I am not coding and developing, you can find me traveling, reading and enjoying outdoor activities with my family and dog.
                 </p>
             </div>
 
--- src/components/Portfolio.js/index.js
+++ src/components/Portfolio.js/index.js
@@ -1,67 +1,61 @@
-import React from 'react';
-import Project from "../Project.js";
+import React, { useState } from 'react';
 
-const projects = [
-    {
-        name: "Car-Dash",
-        picture: "s0",
-        description: "Car Dash is an app that allows users to keep track of repairs and services essential for their vehicles.",
-        link: "https://limitless-reaches-44145.herokuapp.com/",
-        github: "https://github.com/javarrisb/Car-Dash",
-    },
-    {
-        name: "Game-Corner",
-        picture: "s1",
-        description: "An app that allows the user to keep track of their collection of games.",
-        link: "https://afternoon-reef-79835.herokuapp.com/",
-        github: "https://github.com/kerbunker/game-collection"
-    },
-    {
-        name: 'Travel Buddy',
-        picture: "s2",
-        description: "An app for traveler's wanting information for their next journey to a new destination.",
-        link: 'https://khaoulaja.github.io/Travel-Buddy/',
-        github: 'https://github.com/javarrisb/Travel-Buddy'
-    },
-    {
-        name: "Tech About It",
-        picture: "s3",
-        description: "An app that allows developers to publish blog posts and articles.",
-        link: 'https://tech-about-it.herokuapp.com/',
-        github: 'https://github.com/javarrisb/tech-about-it'
-    },
-    {
-        name: "Team Generator",
-        picture: "s4",
-        description: "An app that allows users to input information about employees and generate a webpage that displays the information about each employee.",
-        link: 'https://javarrisb.github.io/Team-Generator/',
-        github: 'https://github.com/javarrisb/Team-Generator'
-    },
-    {
-        name: "Smart Money",
-        picture: "s5",
-        description: "This app allows users to track their bank account deposits and withdrawals while traveling from anywhere around the world. ",
-        link: 'https://blooming-beach-28479.herokuapp.com/',
-        github: 'https://github.com/javarrisb/smart-money'
-    },
-]
+const Portfolio = ({ projects }) => {
+    const [photos] = useState([
+        {
+            name: "Car-Dash",
+            link: "https://limitless-reaches-44145.herokuapp.com/",
+            github: "https://github.com/javarrisb/Car-Dash",
+        },
+        {
+            name: "Game-Corner",
+            link: "https://afternoon-reef-79835.herokuapp.com/",
+            github: "https://github.com/kerbunker/game-collection"
+        },
+        {
+            name: 'Travel Buddy',
+            link: 'https://khaoulaja.github.io/Travel-Buddy/',
+            github: 'https://github.com/javarrisb/Travel-Buddy'
+        },
+        {
+            name: "Tech About It",
+            link: 'https://tech-about-it.herokuapp.com/',
+            github: 'https://github.com/javarrisb/tech-about-it'
+        },
+        {
+            name: "Team Generator",
+            link: 'https://javarrisb.github.io/Team-Generator/',
+            github: 'https://github.com/javarrisb/Team-Generator'
+        },
+        {
+            name: "Smart Money",
+            link: 'https://blooming-beach-28479.herokuapp.com/',
+            github: 'https://github.com/javarrisb/smart-money'
+        },
+    ])
 
-const Portfolio = () => {
+    const currentProjects = photos.filter(photo => photo.projects === projects);
+
     return (
-        <section className="port-section">
-            <div>
-                <h2 className="port-title">My Projects</h2>
-                <div className="row justify-content-around align-items-stretch">
-                    {projects.map((project) => (
-                        <div className="col-sm-12 col-md-6 col-xl-4 p-3 align-self-stretch" key={project.name}>
-                            <Project project={project} />
-                        </div>
-                    ))}
+        <div className="port-container">
+            <h2 className="row justify-content-center">My Work</h2>
+            {currentProjects.map((image, i) => (
+                <div key={image.name} className="port-div">
+                    <h3 className="image-name">{image.name}</h3>
+                    <a href={image.link} target="_blank" rel='noreferrer' className="hrefa">
+                        <img
+                            src={require(`../../assets/images/${i}.png`)}
+                            alt={image.name}
+                            key={image.name}
+                            className="image-project"
+                        />
+                    </a>
+                    <a href={image.github} className="btn btn-success">GH Repo</a>
                 </div>
-            </div>
-        </section>
-    )
-}
+            ))}
+        </div >
+    );
+};
 
 
 export default Portfolio;
