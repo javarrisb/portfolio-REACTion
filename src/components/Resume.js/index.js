f7f952a06dc0f33640523468952ec12ecac46a87
Javarris Barnett <javarrisb@gmail.con>
2022-03-31 12:49:32 -0400

    update

--- src/components/Resume.js/index.js
+++ src/components/Resume.js/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Resume() {
     return (
         <div className='resume-container'>
-            <a href="resume/3-30resume.pdf" className='link-resume' download="Javarris Barnett Resume">Click here to download my resume</a>
+            <a href="resume/Resume.pdf" className='link-resume' download="Javarris Barnett Resume">Click here to download my resume</a>
         </div>
     )
 }
